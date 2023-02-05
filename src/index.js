9301ebee143d86b061264d7810b878f04304a8d9
Vitaly <nelyub86@ukr.net>
2023-02-05 22:51:49 +0200

    in progress

--- src/index.js
+++ src/index.js
@@ -4,19 +4,50 @@ import './css/styles.css';
 const debounce = require('lodash.debounce');
 import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 1500;
 
-fetch('https://restcountries.com/v3.1/name/usa')
-  .then(response => {
-    return response.json();
-  })
-  .then(country => {
-    console.log(country);
-  })
-  .catch(error => {
-    Notiflix.Notify.success('Oops, there is no country with that name');
-    console.log(error);
-  });
+const inputField = document.getElementById('search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+inputField.addEventListener('input', debounce(getInputValue, DEBOUNCE_DELAY));
+
+let currentInputValue = null;
+
+function getInputValue(event) {
+  currentInputValue = event.target.value.trim();
+  console.log(currentInputValue);
+  fetchCountry(currentInputValue);
+}
+
+// fetchCountry(currentInputValue)
+//   .then(renderCountryInfo)
+//   .catch(errorInput)
+//   .finally();
+
+function fetchCountry(country) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${country}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(country => {
+      console.log(country);
+      return country;
+    })
+    .then(renderCountryInfo);
+}
+
+function renderCountryInfo(country) {
+  console.log(country[0]);
+  countryInfo.innerHTML = `<img src=${country[0].flags.svg} width = 60, height = 40><h2>${country[0].name.official}</h2><li>Capital: ${country[0].capital}</li>
+    <li>Population: ${country[0].population}</li><li>Languages: ${country[0].languages}</li>`;
+  //   console.log('ПРИВЕТ Я ИСКОМАЯ СТРАНА');
+}
+
+//
 
 // Notiflix.Notify.success('Oops, there is no country with that name');
 
