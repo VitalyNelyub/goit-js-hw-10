04c9de3ea8f20c4f56be424557e99c4fb58f2e18
Vitaly <nelyub86@ukr.net>
2023-02-05 18:43:12 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+import fetchCountries from './fetchCountries.js';
+
 import './css/styles.css';
+const debounce = require('lodash.debounce');
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch('https://restcountries.com/v3.1/name/usa')
+  .then(response => {
+    return response.json();
+  })
+  .then(country => {
+    console.log(country);
+  })
+  .catch(error => {
+    Notiflix.Notify.success('Oops, there is no country with that name');
+    console.log(error);
+  });
+
+// Notiflix.Notify.success('Oops, there is no country with that name');
+
+// Notiflix.Notify.failure('Oops, there is no country with that name');
+
+// Notiflix.Notify.warning('Oops, there is no country with that name');
+
+// Notiflix.Notify.info('Oops, there is no country with that name');
